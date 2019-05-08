87d71c558356519bdcf2111ecd11151e37b1c868
정호석 <chs1402@nexivil.com>
2019-05-08 15:10:31 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -15,17 +15,9 @@ const ApexPlugIn = (md, opts) => {
 
 
             try {
-                //var prettytmp = tmp.replace(/(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)(['"])?:/g, '$1"$3":').replace("'" , "\"");
-                //console.log(prettytmp)
-                //console.log(JSON.stringify(tmp).replace(/\\n/g, '').replace(/([a-zA-Z0-9-]+):([a-zA-Z0-9-]+)/g, "\"$1\":\"$2\""))
                 const json = JSON.parse(tmp);
-                // console.log(json)
                 var ApexChartsId = 'Apex' + Math.floor(Math.random() * 100) + 1
-                //data-options=${JSON.stringify(json)}
                 return `<div class="apexChart" data-options=${JSON.stringify(json)}  id=${ApexChartsId}  ></div>`
-
-
-
             } catch (err) {
                 console.log(err)
                 return `<pre>${err}</pre>`
@@ -37,11 +29,9 @@ const ApexPlugIn = (md, opts) => {
 }
 
 
-
-
 export const ApexRender = () => {
     var charts = document.querySelectorAll(".apexChart")
-    if(charts != null){
+    if (charts != null) {
 
         charts.forEach((el, index) => {
             var options = JSON.parse(el.dataset.options)
@@ -56,7 +46,7 @@ export default ApexPlugIn
 
 
 
-
+// you can copy and paste this pomo for a test
 // var testOptions = {
 //     "chart": {
 //       "type": "area"
@@ -70,3 +60,7 @@ export default ApexPlugIn
 //       "categories": ["01/01/1991","01/01/1992","01/01/1993","01/01/1994","01/01/1995","01/01/1996","01/01/1997", "01/01/1998","01/01/1999"]
 //     }
 // }
+
+// var test = "```apex
+
+// ```"
