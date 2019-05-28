d61733708881e357894ebe52c5ae40a1928c24d8
정호석 hosuk chung <kirklayer@gmail.com>
2019-05-28 16:43:05 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,19 +47,19 @@ export default ApexPlugIn
 
 
 // you can copy and paste this pomo for a test
-// var testOptions = {
-//     "chart": {
-//       "type": "area"
-//     },
-//     "series": [{
-//       "name": "sales",
-//       "data": [30,40,45,50,49,60,70,91,125]
-//     }],
-//     "xaxis": {
-//       "type": "datetime",
-//       "categories": ["01/01/1991","01/01/1992","01/01/1993","01/01/1994","01/01/1995","01/01/1996","01/01/1997", "01/01/1998","01/01/1999"]
-//     }
-// }
+var testOptions = {
+    "chart": {
+      "type": "area"
+    },
+    "series": [{
+      "name": "sales",
+      "data": [30,40,45,50,49,60,70,91,125]
+    }],
+    "xaxis": {
+      "type": "datetime",
+      "categories": ["01/01/1991","01/01/1992","01/01/1993","01/01/1994","01/01/1995","01/01/1996","01/01/1997", "01/01/1998","01/01/1999"]
+    }
+}
 
 // var test = "```apex
 
